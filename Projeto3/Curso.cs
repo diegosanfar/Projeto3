eddd6b112769647ffe7f16b25f2531ab61752c68
dkingfaria <diegosantosfaria@hotmail.com>
2021-07-21 00:50:13 -0300

    Add project files.

---
+++ Projeto3/Curso.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+    class Curso : Produto, IEstoque
+    {
+        public string autor;
+        private int vagas;
+
+        public Curso(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar vagas no curso {nome}");
+            Console.WriteLine("Digite a quantidade de vagas: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vagas += entrada;
+            Console.WriteLine("Entrada registrada.");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Consumir vagas do curso {nome}");
+            Console.WriteLine("Digite a quantidade de vagas: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vagas -= entrada;
+            Console.WriteLine("Saída registrada.");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vagas restantes: {vagas}");
+            Console.WriteLine("=============================");
+        }
+    }
+}
---
+++ Projeto3/Ebook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook(string nome, float preco, string autor)
+        {
+            this.autor = autor;
+            this.preco = preco;
+            this.nome = nome;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possível dar entrada no estoque de um ebook digital.");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar vendas no Ebook {nome}");
+            Console.WriteLine("Digite a quantidade de vendas: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vendas += entrada;
+            Console.WriteLine("Saída registrada.");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vendas: {vendas}");
+            Console.WriteLine("=============================");
+        }
+    }
+}
